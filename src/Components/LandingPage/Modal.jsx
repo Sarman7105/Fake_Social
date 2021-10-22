5e9cb495bc89034d6f2e9b2c7d2cc5b136d01d2d
Sarman Khurshid Alam <sarman.alam@bjitgroup.com>
2021-10-23 00:34:43 +0600

    registration form validation complete

--- src/Components/LandingPage/Modal.jsx
+++ src/Components/LandingPage/Modal.jsx
@@ -5,37 +5,124 @@ import { useHistory } from 'react-router';
 import './Modal.scss';
 const Modal = (props) => {
 
-    const[name,setName]=useState("");
-    const[email,setEmail]=useState("");
-    const[password,setPassword]=useState("");
-    const[confirmPassword,setConfirmPassword]=useState("");
-    const[date,setDate]=useState(null);
-    const[gender,setGender]=useState("");
-    const history =useHistory();
+    // const[name,setName]=useState("");
+    // const[email,setEmail]=useState("");
+    // const[password,setPassword]=useState("");
+    // const[confirmPassword,setConfirmPassword]=useState("");
+    // const[date,setDate]=useState(null);
+    // const[gender,setGender]=useState("");
+    // const history =useHistory();
+
+    //state for error
+    const[nameError,setNameError]=useState("");
+    const[emailError,setEmailError]=useState("");
+    const[passwordError,setPasswordError]=useState("");
+    const[confirmPasswordError,setConfirmPasswordError]=useState("");
+    const[dateError,setDateError]=useState(null);
+    const[genderError,setGenderError]=useState("");
+
+    //declaring refs for getting input field value
+    const nameRef=useRef();
+    const emailRef=useRef();
+    const passwordRef=useRef();
+    const confirmPasswordRef=useRef();
+    const dateRef=useRef();
+    const maleRef=useRef();
+    const femaleRef=useRef();
+
+    //function for validation
+    const isValid=(name,email,password,confirmPassword,date)=>{
+        let isFormValid=1;
+        if(name.length<4){
+            if(name.length===0){
+                setNameError("Username is required");
+                isFormValid=0;
+            }
+            else{
+
+                setNameError("Username must be at least 6 character");
+                isFormValid=0;
+            }
+        }
+
+        if(password.length<6){
+            if(password.length===0){
+                setPasswordError("Password is required");
+                isFormValid=0;
+            }
+            else{
+
+                setPasswordError("Password must be at least 6 character");
+                isFormValid=0;
+            }
+        }
+
+        if(confirmPassword.length===0){
+
+            setConfirmPasswordError("Retype your password");
+            isFormValid=0;
+
+        }
+
+        else if(password!==confirmPassword){
+
+            setConfirmPasswordError("Password doesn't match");
+            isFormValid=0;
+
+        }
+        if(!email.includes('@')){
+            setEmailError('Enter a valid email address');
+            isFormValid=0;
+        }
+        if(!maleRef.current.checked && !femaleRef.current.checked){
+            setGenderError('Select male or female');
+            isFormValid=0;
+        }
+        if(date.length===0){
+            setDateError('Enter your date of birth');
+            isFormValid=0;
+        }
+
+        return isFormValid;
+    }
+
 
     //form submitting function
     const handleSubmit=(event)=>{
+        setNameError("");
+        setEmailError("");
+        setPasswordError("");
+        setConfirmPasswordError("");
+        setDateError("");
+        setGenderError("");
+        const name=nameRef.current.value;
+        const email=emailRef.current.value;
+        const password=passwordRef.current.value;
+        const confirmPassword=confirmPasswordRef.current.value;
+        const date=dateRef.current.value;
+        const gender=maleRef.current.checked? "Male": "Female";
+        isValid(name,email,password,confirmPassword,date)
 
         const url = "http://localhost:8000/api/v1/register";
         // console.log({name,email,password,confirmPassword,date,gender})
         event.preventDefault();
-        axios
-        .post(url, {
-            name: name,
-            email: email,
-            password: password,
-            password_confirmation: confirmPassword,
-            date_of_birth:date,
-            gender:gender,
+        // axios
+        // .post(url, {
+        //     name: name,
+        //     email: email,
+        //     password: password,
+        //     password_confirmation: confirmPassword,
+        //     date_of_birth:date,
+        //     gender:gender,
         
-        })
-        .then(res => {
-            console.log(res)
+        // })
+        // .then(res => {
+        //     console.log(res)
             
-          })
-          .catch(err => {
-             console.log('caught it!',err.response.data);
-          });
+        //   })
+        //   .catch(err => {
+        //      console.log('caught it!',err.response.data);
+        //   });
     }
 
 
@@ -49,33 +136,7 @@ const Modal = (props) => {
         console.log(name," ",value);
     }
 
-    const handleOnBlur=(e)=>{
-
-        const{name,value}=e.target;
-        if(name==='name'){
-            setName(value);
-        }
-
-        if(name==='email'){
-            setEmail(value);
-        }
-
-        if(name==='password'){
-            setPassword(value);
-        }
-
-        if(name==='confirmPassword'){
-            setConfirmPassword(value);
-        }
-
-        if(name==='date'){
-            setDate(value);
-        }
-
-        if(name==='gender'){
-            setGender(value);
-        }
-    }
+    
     
     return (
         <div className="modalWrapper" >
@@ -86,23 +147,53 @@ const Modal = (props) => {
                 </div>
                 <form  onSubmit={handleSubmit}  className="modalForm">
                     <div className="modalInputElement">
-                        <input onBlur={handleOnBlur}  onChange={handleChange} autoFocus name="name" type="text" placeholder="Enter your name"/>
+                        <input
+                            autoFocus
+                            name="name"
+                            type="text"
+                            placeholder="Enter your name"
+                            ref={nameRef}
+                        />
+                        <p className="errorMessage"> <small> {nameError} </small></p>
                     </div>
 
                     <div className="modalInputElement">
-                        <input  onBlur={handleOnBlur} onChange={handleChange} type="email" name="email" placeholder="Enter your email address"/>
+                        <input
+                            type="email"
+                            name="email"
+                            placeholder="Enter your email address"
+                            ref={emailRef}
+                        />
+                         <p className="errorMessage"> <small> {emailError} </small></p>
                     </div>
 
                     <div className="modalInputElement">
-                        <input onBlur={handleOnBlur}  onChange={handleChange} type="password" name="password" placeholder="Password"/>
+                        <input
+                            type="password"
+                            name="password"
+                            placeholder="Password"
+                            ref={passwordRef}
+                        />
+                         <p className="errorMessage"> <small> {passwordError} </small></p>
                     </div>
 
                     <div className="modalInputElement">
-                        <input onBlur={handleOnBlur}  onChange={handleChange} type="password" name="confirmPassword" placeholder="Retype your password"/>
+                        <input
+                            type="password"
+                            name="confirmPassword"
+                            placeholder="Retype your password"
+                            ref={confirmPasswordRef}
+                        />
+                         <p className="errorMessage"> <small> {confirmPasswordError} </small></p>
                     </div>
                     <div className="modalInputElement">
                         <p className="smallText"> <small>Date of birth ?</small> </p>
-                        <input  onBlur={handleOnBlur} onChange={handleChange} type="date" name="date" placeholder="Password"/>
+                        <input
+                            type="date"
+                            name="date"
+                            ref={dateRef}
+                        />
+                         <p className="errorMessage"> <small> {dateError} </small></p>
                     </div>
 
                     <div className="modalInputElement">
@@ -110,19 +201,37 @@ const Modal = (props) => {
                         <div className="modalRadioContainer">
                             <div className="gender">
                                 <label htmlFor="male">Male</label>
-                                <input onBlur={handleOnBlur}  onChange={handleChange} type="radio" value="male" name="gender" id="male" />
+                                <input
+                                    type="radio"
+                                    value="male"
+                                    name="gender"
+                                    id="male"
+                                    ref={maleRef}
+                                />
+                                
                             </div>
 
                             <div className="gender">
                                 <label htmlFor="female">Female</label>
-                                <input  onBlur={handleOnBlur} onChange={handleChange} type="radio" value="female" name="gender" id="female" />
+                                <input
+                                    type="radio"
+                                    value="female"
+                                    name="gender"
+                                    id="female"
+                                    ref={femaleRef}
+                                />
                             </div>
 
                         </div>
+                        <p className="errorMessage"> <small> {genderError} </small></p>
                     </div>
 
                     <div className="modalInputElement">
-                    <input type="submit" value="Log In" disabled = {false} />
+                    <input
+                        type="submit"
+                        value="SignUp"
+                        disabled = {false}
+                    />
                     </div>
                 </form>
             </div>
