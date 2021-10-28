86ad856d4b79dd98b6a8b2529dc619aa9e79ccf0
Sarman Khurshid Alam <sarman.alam@bjitgroup.com>
2021-10-29 01:45:04 +0600

    working project

--- src/Components/User/User.jsx
+++ src/Components/User/User.jsx
@@ -1,18 +1,33 @@
-import React from "react";
+import axios from "axios";
+import React,{useState,useEffect} from "react";
 import { Link } from "react-router-dom";
 import "./User.scss";
 const User = ({user}) => {
+  const [name,setName]=useState("User");
+  const [image,setImage]=useState("/Assets/person/noAvatar.png");
+  useEffect(()=>{
+        
+    const url=`http://localhost:8000/api/v1/profileByUserId/${user.id}`;
+    axios.get(url)
+    .then(res=>{
+        setName(res.data.user_name)
+        if(res.data.profile_image_url){
+            setImage(res.data.profile_image_url);
+        }
+    })
+    .catch(err=>console.log(err));
+},[])
   return (
     <li className="rightbarFriend">
       <div className="rightbarProfileImageContainer">
         <img
-          src="/Assets/person/2.jpeg"
+          src={image}
           alt=""
           className="rightbarProfileImage"
         />
         <span className="rightbarOnline"></span>
       </div>
-      <Link  to={`/profile/${user.id}`} className="rightbarUserName">Fariha Tasnim</Link>
+      <Link  to={`/profile/${user.id}`} className="rightbarUserName">{name}</Link>
     </li>
   );
 };
